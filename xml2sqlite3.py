2fe9c31eb95a37eb226a3e93087de63fed91f501
Jacob Bieker <jacob@jacobbieker.com>
2014-07-13 10:04:21 -0700

    Add parse_system and parse_star methods

--- xml2sqlite3.py
+++ xml2sqlite3.py
@@ -37,10 +37,12 @@ class Stars(BaseModel):
     radius = DoubleField()
     magV = DoubleField()
     magB = DoubleField()
+    magI = DoubleField()
+    magR = DoubleField()
     magJ = DoubleField()
     magH = DoubleField()
     magK = DoubleField()
-    meetallicity = DoubleField()
+    metallicity = DoubleField()
     spectral_type = TextField()
     temperature = DoubleField()
 
@@ -79,19 +81,76 @@ def create_tables():
 
 #End of table creation and schema of database
 
+def parse_system(system_dict):
+    print("Parse_system Called")
+    current_system = Systems()
+    for entry in system_dict:
+        if entry == "name":
+            current_system.name = entry.value
+        elif entry == "rightascension":
+            current_system.rightAscension = entry.value
+        elif entry == "declination":
+            current_system.declination = entry.value
+        elif entry == "distance":
+            current_system.distance = entry.value
+        elif entry == "epoch":
+            current_system.epoch = entry.value
+        elif entry == "is_system":
+            current_system.is_system = entry.value
+        elif entry == "is_binary":
+            current_system.is_binary = entry.value
+    print("Current_System Saved")
+    current_system.save()
+
+
+def parse_star(star_dict):
+    print("Parse_Star called")
+    current_star = Stars()
+    for entry in star_dict:
+        if entry == "name":
+            current_star.name = entry.value
+        elif entry == "is_star":
+            current_star.is_star = entry.value
+        elif entry == "mass":
+            current_star.mass = entry.value
+        elif entry == "radius":
+            current_star.radius = entry.value
+        elif entry == "metallicity":
+            current_star.metallicity = entry.value
+        elif entry == "spectraltype":
+            current_star.spectral_type = entry.value
+        elif entry == "temperature":
+            current_star.temperature = entry.value
+        elif entry == "magVBRIGHK":
+            current_star.magB = entry.value
+        elif entry == "magV":
+            current_star.magV = entry.value
+        elif entry == "magR":
+            current_star.magR = entry.value
+        elif entry == "magI":
+            current_star.magI = entry.value
+        elif entry == "magG":
+            current_star.magG = entry.value
+        elif entry == "magH":
+            current_star.magH = entry.value
+        elif entry == "magK":
+            current_star.magK = entry.value
+    print("Current_Star Saved")
+    current_star.save()
+
 
 #Start of XML parser
 for file in os.listdir("C:\Development\Resources\Github\open_exoplanet_catalogue\systems") or os.listdir("C:\Development\Resources\Github\open_exoplanet_catalogue\kepler"):
-    count = 0
     if file.endswith(".xml"):
         print(file)
         #Gets all files that end with .xml in directory and parse them
         tree = etree.parse("C:\Development\Resources\Github\open_exoplanet_catalogue\systems" + "\\" + file)
         root = tree.getroot()
-        #Creates a dictionary for each file that starts over with each new file.
+        #Creates a dictionary for each file that starts over with each new file
+        current_system_dict = {}
         for system in root:
-            current_system_dict = {}
-            if system.tag  == "star":
+            #current_system_dict = {}
+            if system.tag == "star":
                 current_system_dict["is_system"] = True
                 current_system_dict["is_binary"] = False
             elif system.tag == "binary":
@@ -99,14 +158,24 @@ for file in os.listdir("C:\Development\Resources\Github\open_exoplanet_catalogue
                 current_system_dict["is_binary"] = True
             else:
                 current_system_dict[system.tag] = system.text
-            print(current_system_dict.keys())
-            for star in system:
+            #print(current_system_dict.items())
+            if system.tag == "star":
                 current_star_dict = {}
-                if star.tag == "planet":
-                    current_star_dict["is_star"] = True
-                else:
-                    current_star_dict[star.tag] = star.text
-               #print(current_star_dict.keys())
-                for planet in star:
-                    current_planet_dict = {planet.tag: planet.text}
-                    #print(current_planet_dict.keys())
+                for star in system:
+                    #current_star_dict = {}
+                    if star.tag == "planet":
+                        current_star_dict["is_star"] = True
+                    else:
+                        current_star_dict[star.tag] = star.text
+                    current_planet_dict = {}
+                #parse_system(current_system_dict)
+                #print(current_star_dict.items())
+                    for planet in star:
+                        current_planet_dict[planet.tag] = planet.text
+                    else:
+                        if current_planet_dict is not False:
+                            print(current_planet_dict.items())
+
+            #print(current_system_dict.items())
+            #print(current_star_dict.items())
+            #print(current_planet_dict.items())
